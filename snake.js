756bb0739e9556cf532b47e73e54572fb87ac790
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-09-26 14:51:21 +0200

    add collision detection
    making animation like in snake game

--- snake.js
+++ snake.js
@@ -3,6 +3,7 @@ const snakeElement = document.querySelector('.snake');
 
 let horizontally = snakeElement.offsetLeft;
 let vertically = snakeElement.offsetTop;
+const refreshTime = 250;
 
 class Snake {
 	position = {
@@ -12,7 +13,7 @@ class Snake {
 
 	velocity = {
 		x: 0,
-		y: 0	
+		y: 0,
 	};
 
 	moveHorizontally() {
@@ -29,36 +30,44 @@ class Snake {
 const snake = new Snake();
 
 function animate() {
-	requestAnimationFrame(animate);
 	snake.moveHorizontally();
 	snake.moveVertically();
+
+	// collision detection
+	if (snake.position.x + 3 < 0 || snake.position.x + 3 > 500) {
+		console.log('You lose');
+	}
+
+	if (snake.position.y + 3 < 0 || snake.position.y + 3 > 500) {
+		console.log('You lose');
+	}
 }
 
-// animate();
+setInterval(animate, refreshTime);
 
 addEventListener('keydown', ({ keyCode }) => {
 	switch (keyCode) {
 		case 37:
-			snake.velocity.x = 1;
+			snake.velocity.x = 20;
 			snake.velocity.y = 0;
 			if (snake.velocity.x > 0) snake.velocity.x = -snake.velocity.x;
 			console.log('Left');
 			break;
 		case 38:
 			snake.velocity.x = 0;
-			snake.velocity.y = 1;
+			snake.velocity.y = 20;
 			if (snake.velocity.y > 0) snake.velocity.y = -snake.velocity.y;
 			console.log('Up');
 			break;
 		case 39:
-			snake.velocity.x = 1;
+			snake.velocity.x = 20;
 			snake.velocity.y = 0;
 			if (snake.velocity.x < 0) snake.velocity.x = -snake.velocity.x;
 			console.log('Right');
 			break;
 		case 40:
 			snake.velocity.x = 0;
-			snake.velocity.y = 1;
+			snake.velocity.y = 20;
 			if (snake.velocity.y < 0) snake.velocity.y = -snake.velocity.y;
 			console.log('Down');
 			break;
