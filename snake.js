0e69cf3310ca9c63d9fbe8b39e4e63a82eaf0c57
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-10-05 21:27:05 +0200

    rewrite the whole snake logic
    I rewrote everything because I was stuck (snake was moving badly)

--- snake.js
+++ snake.js
@@ -1,87 +1,135 @@
-const board = document.querySelector('.board');
-const snakeElement = document.querySelector('.snake');
+const board = document.getElementById('board');
 
-let horizontally = snakeElement.offsetLeft;
-let vertically = snakeElement.offsetTop;
-const refreshTime = 100;
+const SNAKE_SPEED = 5;
+let lastRenderTime = 0;
+let gameOver = false;
+
 let numbers = [];
-
 for (let i = 0; i <= 480; i += 20) {
 	numbers.push(i);
 }
 
-class Snake {
-	position = {
-		x: horizontally,
-		y: vertically,
-	};
+class Input {
+	inputDirection = { x: 0, y: 0 };
+	lastInputDirection = { x: 0, y: 0 };
 
-	velocity = {
-		x: 0,
-		y: 0,
-	};
-
-	moveHorizontally() {
-		this.position.x += this.velocity.x;
-		snakeElement.style.left = this.position.x + 'px';
-
-		return this.position.x;
-	}
-
-	moveVertically() {
-		this.position.y += this.velocity.y;
-		snakeElement.style.top = this.position.y + 'px';
-
-		return this.position.y;
+	getInputDirection() {
+		this.lastInputDirection = this.inputDirection;
+		return this.inputDirection;
 	}
 }
 
-class SnakeTail extends Snake {
+class Snake extends Input {
+	snakeBody = [{ x: 240, y: 240 }];
+	newSegments = 0;
+	rotate = 'rotate(0deg)';
+
+	draw() {
+		board.innerHTML = '';
+
+		this.snakeBody.forEach((segment, index) => {
+			const snakeElement = document.createElement('div');
+			snakeElement.style.left = segment.x + 'px';
+			snakeElement.style.top = segment.y + 'px';
+			snakeElement.className = 'snake_tail';
+			board.appendChild(snakeElement);
+
+			if (index === 0) {
+				snakeElement.style.transform = this.rotate;
+				snakeElement.className = 'snake';
+			}
+		});
+	}
+
+	update() {
+		this.addSegments();
+
+		const snakeHeadPos = this.getInputDirection();
+		for (let i = this.snakeBody.length - 2; i >= 0; i--) {
+			this.snakeBody[i + 1] = { ...this.snakeBody[i] };
+		}
+
+		this.snakeBody[0].x += snakeHeadPos.x;
+		this.snakeBody[0].y += snakeHeadPos.y;
+	}
+
+	expandSnake(amount) {
+		snake.newSegments += amount;
+	}
+
+	onSnake(position, { ignoreHead = false } = {}) {
+		return snake.snakeBody.some((segment, index) => {
+			if (ignoreHead && index === 0) return false;
+			return snake.equalPosition(segment, position);
+		});
+	}
+
+	snakeHead() {
+		return this.snakeBody[0];
+	}
+
+	snakeIntersection() {
+		return this.onSnake(this.snakeBody[0], { ignoreHead: true });
+	}
+
+	outsiderBoard() {
+		return (
+			this.snakeHead().x < 0 ||
+			this.snakeHead().x > 480 ||
+			this.snakeHead().y < 0 ||
+			this.snakeHead().y > 480
+		);
+	}
+
+	equalPosition(pos1, pos2) {
+		return pos1.x === pos2.x && pos1.y === pos2.y;
+	}
+
+	addSegments() {
+		for (let i = 0; i < this.newSegments; i++) {
+			this.snakeBody.push({ ...this.snakeBody[this.snakeBody.length - 1] });
+		}
+
+		this.newSegments = 0;
+	}
+}
+
+class Candy extends Snake {
 	constructor() {
 		super();
+		this.food = {
+			x: this.getRandomCandyPosition(),
+			y: this.getRandomCandyPosition(),
+		};
 	}
 
-	velocityTail = {
-		x: 0,
-		y: 0,
-	};
-	allTails = [];
-
-	render() {
-		const snakeTail = document.createElement('div');
-		snakeTail.className = 'snake_tail';
-		// snakeTail.style.left = this.position.x + 'px';
-		// snakeTail.style.top = this.position.y + 'px';
-
-		this.allTails.push(snakeTail);
-		return snakeTail;
+	draw() {
+		const candyEl = document.createElement('div');
+		candyEl.style.left = this.food.x + 'px';
+		candyEl.style.top = this.food.y + 'px';
+		candyEl.className = 'candy';
+		candyEl.style['background-color'] = `rgb(
+      ${this.getRandomInt(255)},
+			0,
+			${this.getRandomInt(255)}
+      )`;
+		board.appendChild(candyEl);
 	}
 
-	moveTail(x, y) {
-		if (this.allTails.length > 0) {
-			for (let i = 0; i < this.allTails.length; i++) {
-				this.allTails[i].style.left = x - this.velocityTail.x * (i + 1) + 'px';
-				this.allTails[i].style.top = y - this.velocityTail.y * (i + 1) + 'px';
-			}
+	update() {
+		if (this.onSnake(this.food)) {
+			this.expandSnake(1);
+			this.food.x = this.getRandomCandyPosition();
+			this.food.y = this.getRandomCandyPosition();
 		}
 	}
-}
 
-class Candy {
-	render() {
-		const candyEl = document.createElement('div');
-		candyEl.className = 'candy';
-		candyEl.style.left =
-			numbers[Math.floor(Math.random() * (numbers.length - 1))] + 'px';
-		candyEl.style.top =
-			numbers[Math.floor(Math.random() * (numbers.length - 1))] + 'px';
-		candyEl.style['background-color'] = `rgb(
-			${this.getRandomInt(255)},
-			${this.getRandomInt(255)},
-			${this.getRandomInt(255)}
-		)`;
-
-		return candyEl;
+	getRandomCandyPosition() {
+		let newCandyPosition;
+		while (newCandyPosition == null || this.onSnake(newCandyPosition)) {
+			newCandyPosition = numbers[this.getRandomInt(24)];
+		}
+		return newCandyPosition;
 	}
 
 	getRandomInt(max) {
@@ -89,100 +137,66 @@ class Candy {
 	}
 }
 
-// init
-const candy = new Candy();
 const snake = new Snake();
-const snakeTail = new SnakeTail();
+const candy = new Candy();
 
-function animate() {
-	snake.moveHorizontally();
-	snake.moveVertically();
-	snakeTail.moveTail(snake.position.x, snake.position.y);
-
-	if (board.lastChild.className !== 'candy') {
-		board.appendChild(candy.render());
-	} else {
-		const candy = document.querySelector('.candy');
-
-		if (
-			snake.position.x + 'px' === candy.style.left &&
-			snake.position.y + 'px' === candy.style.top
-		) {
-			// tutaj renderowac nowy element
-			snakeElement.insertAdjacentElement('afterend', snakeTail.render());
-			board.removeChild(candy);
-			console.log(snakeTail.allTails);
-		}
-	}
-
-	// if (snakeTailHandler.length > 0) {
-	// 		if (
-	// 			document.querySelectorAll('.snake_tail').length <
-	// 			snakeTailHandler.length
-	// 		) {
-	// 			snakeElement.insertAdjacentElement('afterend', snakeTail.render());
-	// 		}
-	// }
-
-	// collision detection
-	if (snake.position.x + 3 < 0 || snake.position.x + 3 > 500) {
-		console.log('You lose');
-	}
-
-	if (snake.position.y + 3 < 0 || snake.position.y + 3 > 500) {
-		console.log('You lose');
-	}
+function checkDeath() {
+	gameOver =
+		snake.outsiderBoard(snake.snakeBody[0]) || snake.snakeIntersection();
 }
 
-setInterval(animate, refreshTime);
+function update() {
+	snake.update();
+	candy.update();
+	checkDeath();
+}
+
+function draw() {
+	snake.draw();
+	candy.draw();
+}
+
+function animate(currentTime) {
+	if (gameOver) {
+		if (confirm('You lost. Press ok to restart.')) {
+			location = '/';
+		}
+		return;
+	}
+
+	requestAnimationFrame(animate);
+	const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+	if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+	lastRenderTime = currentTime;
+
+	update();
+	draw();
+}
+
+requestAnimationFrame(animate);
 
 addEventListener('keydown', ({ keyCode }) => {
 	switch (keyCode) {
 		case 37:
-			if (snake.velocity.x > 0) break;
-			if (snake.velocity.x === 0) {
-				snake.velocity.y = 0;
-				snake.velocity.x = 20;
-				snake.velocity.x = -snake.velocity.x;
-				snakeElement.style.transform = 'rotate(180deg)';
-				snakeTail.velocityTail.x = -20;
-				snakeTail.velocityTail.y = 0;
-			}
-			// console.log('Left');
+			if (snake.lastInputDirection.x !== 0) break;
+			snake.inputDirection = { x: -20, y: 0 };
+			snake.rotate = 'rotate(180deg)';
 			break;
 		case 38:
-			if (snake.velocity.y > 0) break;
-			if (snake.velocity.y === 0) {
-				snake.velocity.x = 0;
-				snake.velocity.y = 20;
-				snake.velocity.y = -snake.velocity.y;
-				snakeElement.style.transform = 'rotate(-90deg)';
-				snakeTail.velocityTail.x = 0;
-				snakeTail.velocityTail.y = -20;
-			}
-			// console.log('Up');
+			if (snake.lastInputDirection.y !== 0) break;
+			snake.inputDirection = { x: 0, y: -20 };
+			snake.rotate = 'rotate(-90deg)';
 			break;
 		case 39:
-			if (snake.velocity.x > 0) break;
-			if (snake.velocity.x === 0) {
-				snake.velocity.y = 0;
-				snake.velocity.x = 20;
-				snakeElement.style.transform = 'rotate(0deg)';
-				snakeTail.velocityTail.x = 20;
-				snakeTail.velocityTail.y = 0;
-			}
-			// console.log('Right');
+			if (snake.lastInputDirection.x !== 0) break;
+			snake.inputDirection = { x: 20, y: 0 };
+			snake.rotate = 'rotate(0deg)';
 			break;
 		case 40:
-			if (snake.velocity.y < 0) break;
-			if (snake.velocity.y === 0) {
-				snake.velocity.x = 0;
-				snake.velocity.y = 20;
-				snakeElement.style.transform = 'rotate(90deg)';
-				snakeTail.velocityTail.x = 0;
-				snakeTail.velocityTail.y = 20;
-			}
-			// console.log('Down');
+			if (snake.lastInputDirection.y !== 0) break;
+			snake.inputDirection = { x: 0, y: 20 };
+			snake.rotate = 'rotate(90deg)';
 			break;
 	}
 });
